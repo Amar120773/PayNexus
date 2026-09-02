8f325a3c2129ccc48367c89657c90f8b6e329d2a
Amar <gkmamarnath@gmail.com>
2026-09-02 16:49:54 +0530

    Delete OneDrive/Desktop/ClusterFlow – Distributed Compute Job Scheduler directory

--- OneDrive/Desktop/ClusterFlow – Distributed Compute Job Scheduler/frontend/src/features/workers/services/workersApi.ts
+++
@@ -1,9 +0,0 @@
-import api from '../../../services/api';
-import { WorkerNode } from '../../../types';
-
-export const workersApi = {
-  list: async (): Promise<WorkerNode[]> => {
-    const response = await api.get<WorkerNode[]>('/workers');
-    return response.data;
-  },
-};
